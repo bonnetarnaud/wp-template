5bbb981262353a5b04356d8173a4607df586d133
arnaud bonnet <2a.bonnet@gmail.com>
2022-06-09 12:36:27 +0200

    add sh script

---
+++ config.sh
@@ -0,0 +1,7 @@
+# Base URL (sites will be setup as subdirectories)
+BASE_URL="http://localhost:8888"
+
+# Database information
+DB_USER="root"
+DB_PASS="root"
+DB_HOST="localhost"
---
+++ install-wp.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+# Includes your config file
+source config.sh
+
+if [ $# -ne 1 ]; then
+    echo $0: usage: Destination Name
+    exit 1
+fi
+
+DEST=$1
+
+
+# Download WP Core.
+wp core download --locale=fr_FR
+
+# Generate the wp-config.php file
+wp core config --dbhost=localhost --dbname=$DEST --dbuser=$DB_USER --dbpass=$DB_PASS --dbprefix=wps_ --locale=fr_FR --extra-php <<PHP
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
+PHP
+
+#install db
+wp db create --dbuser=$DB_USER --dbpass=$DB_PASS
+
+# Install the WordPress database.
+wp core install --url=$BASE_URL/$DEST --title=$DEST --admin_user=admin --admin_password=admin --admin_email=admin@admin.fr
+
+# set debug to true
+wp config set WP_DEBUG true --raw
+
+# DELETE README.HTML
+echo Deleting readme.html
+rm -f readme.html
+
+# DELETE LICENCE.TXT
+echo Deleting licence.txt
+rm -f license.txt
+
+echo Deleting junk plugins...
+rm -r wp-content/plugins/hello.php
+rm -r wp-content/plugins/akismet
+echo
+
+
+open $BASE_URL/$DEST
